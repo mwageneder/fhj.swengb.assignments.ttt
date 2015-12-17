51cca0de33212bbf7c48ac9ce9abb00296253614
maxwageneder <maximilian.wageneder@edu.fh-joanneum.at>
2015-12-17 22:31:22 +0100

    implemented: - object & case class methods for ttt game - GUI implementation ( gameField, Button to start, Message Label for game status) - Player 1 vs Player 2 works fine now - combined GUI & Moves with MouseClickEvents - additional css ... still working on improvements

---
+++ src/main/scala/fhj/swengb/assignments/ttt/mwageneder/TicTacToe.scala
@@ -0,0 +1,253 @@
+package fhj.swengb.assignments.ttt.mwageneder
+
+import scala.collection.Set
+
+/**
+  * models the different moves the game allows
+  *
+  * each move is made by either player a or player b.
+  */
+
+sealed trait TMove {
+  def idx: Int
+}
+
+case object TopLeft extends TMove {
+  override def idx: Int = 0
+}
+
+case object TopCenter extends TMove {
+  override def idx: Int = 1
+}
+
+case object TopRight extends TMove {
+  override def idx: Int = 2
+}
+
+case object MiddleLeft extends TMove {
+  override def idx: Int = 3
+}
+
+case object MiddleCenter extends TMove {
+  override def idx: Int = 4
+}
+
+case object MiddleRight extends TMove {
+  override def idx: Int = 5
+}
+
+case object BottomLeft extends TMove {
+  override def idx: Int = 6
+}
+
+case object BottomCenter extends TMove {
+  override def idx: Int = 7
+}
+
+case object BottomRight extends TMove {
+  override def idx: Int = 8
+}
+
+
+/**
+  * for a tic tac toe game, there are two players, player A and player B
+  */
+
+sealed trait Player
+
+case object PlayerA extends Player
+
+case object PlayerB extends Player
+
+object TicTacToe {
+
+  /**
+    * creates an empty tic tac toe game
+    * @return
+    */
+
+  def apply(): TicTacToe = TicTacToe(Map())
+
+  /**
+    * For a given tic tac toe game, this function applies all moves to the game.
+    * The first element of the sequence is also the first move.
+    *
+    * @param t
+    * @param moves
+    * @return
+    */
+
+  def play(t: TicTacToe, moves: Seq[TMove]): TicTacToe = {
+
+    var player: Player = PlayerA
+
+    for (move <- moves){
+      t.turn(move, player)
+
+      if(player.equals(PlayerA))
+        player = PlayerB
+      else
+        player = PlayerA
+    }
+    return t
+  }
+
+  /**
+    * creates all possible games.
+    * @return
+    */
+
+  def mkGames(): Map[Seq[TMove], TicTacToe] = {
+
+    ???
+
+  }
+
+}
+
+/**
+  * Models the well known tic tac toe game.
+  *
+  * The map holds the information which player controls which field.
+  *
+  * The nextplayer parameter defines which player makes the next move.
+  */
+
+case class TicTacToe(moveHistory: Map[TMove, Player],
+                     nextPlayer: Player = PlayerA) {
+
+  /**
+    * outputs a representation of the tic tac toe like this:
+    *
+    * |---|---|---|
+    * | x | o | x |
+    * |---|---|---|
+    * | o | x | x |
+    * |---|---|---|
+    * | x | o | o |
+    * |---|---|---|
+    *
+    *
+    * @return
+    */
+
+  def asString(): String = {
+
+
+    val indexMap = Map(0->16, 1->20, 2->24, 3->44, 4->48, 5->52, 6->72, 7->76, 8->80)
+
+
+    var field: String = "|---|---|---|\n" +
+                        "|   |   |   |\n" +
+                        "|---|---|---|\n" +
+                        "|   |   |   |\n" +
+                        "|---|---|---|\n" +
+                        "|   |   |   |\n" +
+                        "|---|---|---|\n"
+
+    val pos = Map(0->16, 1->20, 2->24, 3->44, 4->48, 5->52, 6->72, 7->76, 8->80)
+
+
+    for ((a, b) <- moveHistory) {
+      if (b == PlayerA) {
+        field = field.updated(indexMap(a.idx), "O").mkString
+      }
+      else if (b == PlayerB) {
+        field = field.updated(indexMap(a.idx), "X").mkString
+      }
+      else {
+        field = field.updated(indexMap(a.idx), "").mkString
+      }
+    }
+    field
+  }
+
+
+  val winnings: List[Set[TMove]] = List(Set(TopLeft, TopCenter, TopRight),
+                                        Set(MiddleLeft, MiddleCenter, MiddleRight),
+                                        Set(BottomLeft, BottomCenter, BottomRight),
+                                        Set(TopLeft, MiddleCenter, BottomRight),
+                                        Set(TopRight, MiddleCenter, BottomLeft),
+                                        Set(TopCenter, MiddleCenter, BottomCenter),
+                                        Set(TopLeft, MiddleLeft, BottomLeft),
+                                        Set(TopRight, MiddleRight, BottomRight))
+
+
+  def checkIfWon(player: Player): Boolean = {
+
+    if(winnings.head.subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(1).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(2).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(3).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(4).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(5).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(6).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else if(winnings(7).subsetOf(moveHistory.filter(_._2 == player).keySet))
+      true
+    else
+      false
+  }
+
+  /**
+   * the moves which are still to be played on this tic tac toe.
+   */
+
+  val playedMoves: Set[TMove] = Set(TopLeft, TopCenter, TopRight, MiddleLeft, MiddleCenter, MiddleRight, BottomLeft, BottomCenter, BottomRight)
+
+  val unsetFields = moveHistory.filter(x => x._2 != PlayerA || x._2 != PlayerB).keySet
+
+  val remainingMoves: Set[TMove] = playedMoves.diff(unsetFields)
+
+
+  val gameOver: Boolean = true match {
+    case gameEnd => winner.isDefined || remainingMoves.isEmpty
+    case _ => false
+  }
+
+  /**
+    * given a tic tac toe game, this function returns all
+    * games which can be derived by making the next turn. that means one of the
+    * possible turns is taken and added to the set.
+    */
+
+  lazy val nextGames: Set[TicTacToe] = {
+
+    ???
+
+  }
+
+  /**
+    * Either there is no winner, or PlayerA or PlayerB won the game.
+    *
+    * The set of moves contains all moves which contributed to the result.
+    */
+
+  def winner: Option[(Player, Set[TMove])] = {
+
+    if(checkIfWon(PlayerA))
+      Some(PlayerA,moveHistory.filter(_._2 == PlayerA).keySet)
+    else if(checkIfWon(PlayerB))
+      Some(PlayerB,moveHistory.filter(_._2 == PlayerB).keySet)
+    else None
+  }
+
+  def turn(p: TMove, player: Player): TicTacToe = {
+    if(!moveHistory.contains(p)){
+      if(player.equals(PlayerA))
+        TicTacToe(moveHistory + (p -> player), PlayerB)
+      else
+        TicTacToe(moveHistory + (p -> player))
+    }
+    else
+      TicTacToe(moveHistory)
+  }
+}
+
+
---
+++ src/main/scala/fhj/swengb/assignments/ttt/mwageneder/TicTacToeApp.scala
@@ -0,0 +1,162 @@
+package fhj.swengb.assignments.ttt.mwageneder
+
+import java.net.URL
+import java.util.ResourceBundle
+import javafx.application.Application
+import javafx.event.EventHandler
+import javafx.fxml.{FXML, Initializable, FXMLLoader}
+import javafx.scene.control.{Button, Label}
+import javafx.scene.input.MouseEvent
+import javafx.scene.layout.{GridPane}
+import javafx.scene.{Scene, Parent}
+import javafx.stage.Stage
+
+import scala.util.control.NonFatal
+
+object TicTacToeApp {
+  def main(args: Array[String]) {
+    Application.launch(classOf[TicTacToeApp], args: _*)
+  }
+}
+
+class TicTacToeApp extends javafx.application.Application {
+
+  val Fxml = "/fhj/swengb/assignments/ttt/TicTacToeApp.fxml"
+
+  val loader = new FXMLLoader(getClass.getResource(Fxml))
+
+  override def start(stage: Stage): Unit = try {
+    stage.setTitle("Maxl's TicTacToe Game")
+    loader.load[Parent]() // side effect
+    val scene = new Scene(loader.getRoot[Parent])
+    stage.setScene(scene)
+    stage.show()
+
+  } catch {
+    case NonFatal(e) => {
+      e.printStackTrace()
+    }
+  }
+}
+
+class TicTacToeAppController extends Initializable {
+
+  @FXML var gridpane : GridPane = _
+
+  @FXML var topleft: Label = _
+  @FXML var topcenter: Label = _
+  @FXML var topright: Label = _
+  @FXML var middleleft: Label = _
+  @FXML var middlecenter: Label = _
+  @FXML var middleright: Label = _
+  @FXML var bottomleft: Label = _
+  @FXML var bottomcenter: Label = _
+  @FXML var bottomright: Label = _
+
+  @FXML var msglabel: Label = _
+  @FXML var btnnewgame: Button = _
+
+
+  override def initialize(location: URL, resources: ResourceBundle): Unit = {
+
+    topleft.setOnMouseClicked(mouseEventHandler) ; topleft.setUserData(0)
+    topcenter.setOnMouseClicked(mouseEventHandler); topcenter.setUserData(1)
+    topright.setOnMouseClicked(mouseEventHandler); topright.setUserData(2)
+    middleleft.setOnMouseClicked(mouseEventHandler); middleleft.setUserData(3)
+    middlecenter.setOnMouseClicked(mouseEventHandler); middlecenter.setUserData(4)
+    middleright.setOnMouseClicked(mouseEventHandler) ; middleright.setUserData(5)
+    bottomleft.setOnMouseClicked(mouseEventHandler) ; bottomleft.setUserData(6)
+    bottomcenter.setOnMouseClicked(mouseEventHandler) ; bottomcenter.setUserData(7)
+    bottomright.setOnMouseClicked(mouseEventHandler) ;  bottomright.setUserData(8)
+
+    gridpane.setDisable(true)
+    btnnewgame.setOnMouseClicked(mouseEventHandler)
+    msglabel.setText("To start a New Game click the Button")
+  }
+
+  var newGame = TicTacToe.apply()
+
+  val labelmoveMap: Map[Int, (Label, TMove)] = Map( 0 -> (topleft,TopLeft),
+                                                    1 -> (topcenter,TopCenter),
+                                                    2 -> (topright,TopRight),
+                                                    3 -> (middleleft,MiddleLeft),
+                                                    4 -> (middlecenter,MiddleCenter),
+                                                    5 -> (middleright,MiddleRight),
+                                                    6 -> (bottomleft,BottomLeft),
+                                                    7 -> (bottomcenter,BottomCenter),
+                                                    8 -> (bottomright,BottomRight))
+
+  val mouseEventHandler: EventHandler[_ >: MouseEvent] = new EventHandler[MouseEvent] {
+
+    override def handle(event: MouseEvent): Unit = {
+      event.getSource match {
+        case onclick: Label => {
+
+          if (newGame.gameOver)
+            gameOver()
+
+          if (newGame.nextPlayer == PlayerA) {
+            val a = labelmoveMap.get(onclick.getUserData.toString.toInt)
+
+            if(newGame.remainingMoves.contains(a.get._2)){
+              onclick.setText("x")
+              newGame = newGame.turn(a.get._2,newGame.nextPlayer)
+              gameOver()
+            }
+          }
+
+          else if (newGame.nextPlayer == PlayerB) {
+
+            if(newGame.remainingMoves.contains(labelmoveMap(onclick.getUserData.toString.toInt)._2)){
+              onclick.setText("o")
+              newGame = newGame.turn(labelmoveMap(onclick.getUserData.toString.toInt)._2,newGame.nextPlayer)
+              gameOver()
+
+            }
+          }
+        }
+
+        case btnGame: Button if btnnewgame == btnnewgame => {
+
+          newGame = TicTacToe.apply()
+          gridpane.setDisable(false)
+
+          bottomcenter.setText(null)
+          bottomleft.setText(null)
+          bottomright.setText(null)
+          middleleft.setText(null)
+          middlecenter.setText(null)
+          middleright.setText(null)
+          topleft.setText(null)
+          topcenter.setText(null)
+          topright.setText(null)
+          msglabel.setText("New Game started")
+        }
+
+        case _ => assert(false)
+      }
+    }
+  }
+
+  def gameOver(): Unit = {
+
+    if (newGame.gameOver){
+
+      if(!newGame.checkIfWon(PlayerA) && !newGame.checkIfWon(PlayerB)) {
+        msglabel.setText("Draw!")
+        gridpane.setDisable(true)
+      }
+      else if(newGame.winner.get._1.equals(PlayerA)) {
+        msglabel.setText("X - Player 1 won!")
+        gridpane.setDisable(true)
+      }
+      else if(newGame.winner.get._1.equals(PlayerB)) {
+        msglabel.setText("O - Player 2 won!")
+        gridpane.setDisable(true)
+      }
+    }
+  }
+}
+
+
+// WAS SOLL DAS WIESO PUSHT DU DAS NICHT ! ? aaaaaahhh
